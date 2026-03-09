a05626ec5f7a6e68a9273ac8d376bcb1a9024ff7
leitao1974 <luiscrleitao@gmail.com>
2026-03-09 17:01:42 +0000

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-
-class Config:
-    APP_NAME = "Sistema Nacional de Fiscalização Ambiental"
-    DATABASE = "fiscalizacao.db"
-
-    REN_LAYER = "data/ren.geojson"
-    RAN_LAYER = "data/ran.geojson"
-    NATURA_LAYER = "data/natura2000.geojson"
